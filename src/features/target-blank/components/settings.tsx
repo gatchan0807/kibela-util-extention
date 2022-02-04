5f95e47cd0aae23059b7d233679a951fe93f1426
gatchan0807 <yuki.dolphin.koga@gmail.com>
2022-02-05 02:52:03 +0900

    feat: target-blank設定用コンポーネントにスタイル当て

--- src/features/target-blank/components/settings.tsx
+++ src/features/target-blank/components/settings.tsx
@@ -1,18 +1,39 @@
-import React from "react"
+import React from 'react';
 
 export const Settings: React.FC = () => {
-    return (
-        <div>
-            <input type="checkbox" name="always-open-another-tab" id="always-open-another-tab" checked />
-            <label htmlFor="always-open-another-tab">デフォルトでリンクを別タブで開く</label>
-            <hr />
-            <div>
-                <label htmlFor="url-pattern-form">URLが下記パターンに一致する場合は別タブで開かない</label>
-                <input type="text" name="url-pattern-form" id="url-pattern-form" placeholder="https://*.example.com/*" />
-                <ul>
-                    <li>https://*.example.com/*</li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className='text-gray-800'>
+      <h2 className='text-lg border-b-2 border-cyan-500 pl-2'>リンク先設定</h2>
+      <div className="py-2 text-base">
+        <input
+          className="mx-2"
+          type="checkbox"
+          name="always-open-another-tab"
+          id="always-open-another-tab"
+          checked
+        />
+        <label className='hover:cursor-pointer' htmlFor="always-open-another-tab">
+          デフォルトでリンクを別タブで開く
+        </label>
+      </div>
+      <div className='w-11/12 mx-auto border-b border-gray-300'>{/* spacer */}</div>
+      <div className='py-2 text-base'>
+        <h3 className='text-base mb-4 pl-2 border-b border-cyan-500'>例外指定</h3>
+        <input
+          className='block w-full px-2 mb-2 border border-cyan-500 rounded'
+          type="text"
+          name="url-pattern-form"
+          id="url-pattern-form"
+          placeholder="https://*.example.com/*"
+        />
+        <p className='text-xs text-cyan-600 leading-tight whitespace-nowrap'>
+          *リンク先のURLが<wbr />下記パターンに一致する場合は<wbr />別タブで開かない
+        </p>
+        <ul className='text-sm mt-2'>
+          <li className='ml-6 pb-1 list-disc'>https://*.example.com/*</li>
+          <li className='ml-6 pb-1 list-disc'>https://*.example.com/*</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
