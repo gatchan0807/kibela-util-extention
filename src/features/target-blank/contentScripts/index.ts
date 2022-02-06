3281e7b4dc34ffaaea8001893ea3b8708e5a6e48
gatchan0807 <yuki.dolphin.koga@gmail.com>
2022-02-06 19:21:50 +0900

    refactor: Element要素のリネームと定義の順序を読みやすいように変更

--- src/features/target-blank/contentScripts/index.ts
+++ src/features/target-blank/contentScripts/index.ts
@@ -1,16 +1,15 @@
-export const allATag = document.querySelectorAll(".markdown-body a[href]")
-export const domElement: DOMElement[] = []
-
 export type DOMElement = {
-    element: Element,
     href: string,
     className: string,
     idName: string,
     isSameSiteLink: boolean,
     isUserMention: boolean,
     isAnchorLink: boolean,
+    rawElement: Element,
 }
 
+export const domElement: DOMElement[] = []
+const allATag = document.querySelectorAll(".markdown-body a[href]")
 allATag.forEach(e => {
     const className = e.getAttribute("class") ?? ""
     const isAnchorLink = className.includes("anchor")
@@ -21,9 +20,9 @@ allATag.forEach(e => {
         href: e.getAttribute("href") ?? "",
         className: e.getAttribute("class") ?? "",
         idName: e.getAttribute("id") ?? "",
-        isSameSiteLink, 
+        isSameSiteLink,
         isUserMention,
         isAnchorLink,
-        element: e,
+        rawElement: e,
     })
 })
