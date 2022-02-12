5de72b3f99929f9bc52a35759ec22fbe5b799e95
gatchan0807 <yuki.dolphin.koga@gmail.com>
2022-02-13 01:03:21 +0900

    chore: コンソールに表示するログを変更

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -2,8 +2,8 @@ import { setTargetBlank } from "../features/target-blank/contentScripts"
 import { getSettingsAboutTargetBlank } from "../features/target-blank/utils/getSettingsAboutTargetBlank";
 
 (async () => {
-    console.log("hello")
-
+    console.log("Running 木べら！！ on feature of TargetBlank");
+    
     const settings = await getSettingsAboutTargetBlank()
     await setTargetBlank(settings)
 })()
