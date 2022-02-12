8c293b2a751f601dde2a6d8f29e8abe40f80a3e9
gatchan0807 <yuki.dolphin.koga@gmail.com>
2022-02-13 00:57:04 +0900

    chore: 不要な処理の削除

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -3,16 +3,9 @@ import browser from 'webextension-polyfill';
 import * as SettingUI from './settings';
 
 export const Popup: React.VFC = () => {
-  const handleClick = () => {
-    browser.tabs.create({ url: 'https://example.com/' });
-  };
-
-  // a button to open example.com
   return (
     <div className='popup-wrapper'>
       <SettingUI.TargetBlankSettings></SettingUI.TargetBlankSettings>
-      <hr />
-      <button onClick={handleClick}>Button</button>
     </div>
   )
 };
