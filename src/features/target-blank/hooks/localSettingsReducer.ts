3ffcc924e6665ec5c3f96dbd56b561ce62efd9d8
gatchan0807 <yuki.dolphin.koga@gmail.com>
2022-03-21 17:49:38 +0900

    fix: 型を設定できることで不要になった条件式・代入処理を削除

--- src/features/target-blank/hooks/localSettingsReducer.ts
+++ src/features/target-blank/hooks/localSettingsReducer.ts
@@ -18,38 +18,33 @@ export type State = {
 
 export const localSettingsReducer = (state: State, action: Action): State => {
     if (action.type === "setAlwaysOpenOtherTab") {
-        const payloadBoolean = typeof action.payload === "boolean" ? action.payload : false
         return {
             ...state,
-            alwaysOpenOtherTab: payloadBoolean
+            alwaysOpenOtherTab: action.payload
         }
     }
     if (action.type === "setInKibelaLinkOpenSameTab") {
-        const payloadBoolean = typeof action.payload === "boolean" ? action.payload : false
         return {
             ...state,
-            inKibelaLinkOpenSameTab: payloadBoolean
+            inKibelaLinkOpenSameTab: action.payload
         }
     }
     if (action.type === "setExcludeUrlInputValidation") {
-        const payloadString = typeof action.payload === "string" ? action.payload : ""
         return {
             ...state,
-            excludeUrlInputValidation: payloadString
+            excludeUrlInputValidation: action.payload
         }
     }
     if (action.type === "setExcludeUrlInput") {
-        const payloadString = typeof action.payload === "string" ? action.payload : ""
         return {
             ...state,
-            excludeUrlInput: payloadString
+            excludeUrlInput: action.payload
         }
     }
     if (action.type === "setExcludeUrlList") {
-        const payloadArray = Array.isArray(action.payload) ? action.payload : []
         return {
             ...state,
-            excludeUrlList: payloadArray
+            excludeUrlList: action.payload
         }
     }
     return state;
