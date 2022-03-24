64c47a33899f0a26ac56899f92064de824f70104
gatchan0807 <yuki.dolphin.koga@gmail.com>
2022-03-25 02:10:28 +0900

    refactor: Observeロジックの呼び出し方の変更

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -1,9 +1,11 @@
 import { setTargetBlank } from "../features/target-blank/contentScripts"
+import { startPreviewBoxObserve } from "../features/target-blank/contentScripts/startPreviewBoxObserve";
 import { getSettingsAboutTargetBlank } from "../features/target-blank/utils/getSettingsAboutTargetBlank";
 
 (async () => {
     console.log("Running 木べら！！ on feature of TargetBlank");
     
     const settings = await getSettingsAboutTargetBlank()
+    startPreviewBoxObserve(settings)
     await setTargetBlank(settings)
 })()
