8ec7cd811677079571a5a435f15bc508285dfa87
gatchan0807 <yuki.dolphin.koga@gmail.com>
2022-03-25 02:07:47 +0900

    chore: 型情報を共有部分に追加（移動）

--- src/features/target-blank/types/index.ts
+++ src/features/target-blank/types/index.ts
@@ -9,3 +9,17 @@ export type ExcludeUrl = {
     url: string;
     host: string;
 };
+
+export type DOMElement = {
+    href: string,
+    className: string,
+    idName: string,
+    isSameSiteLink: boolean,
+    isUserMention: boolean,
+    isAnchorLink: boolean,
+    hasTargetBlank: boolean,
+    setTargetBlankAttribute: (e: Element) => void,
+    rawElement: Element,
+}
+
+export type EffectsDomFunction = ({ elements, settings }: { elements: DOMElement[], settings: TargetBlankSettings }) => void
