6c410f02227015787c31861366bb41ecc19854f7
gatchan0807 <yuki.dolphin.koga@gmail.com>
2022-11-15 17:57:12 +0900

    test: テスト名と使用するサンプルデータの修正

--- src/features/template-search/contentScripts/sha256.test.ts
+++ src/features/template-search/contentScripts/sha256.test.ts
@@ -9,16 +9,16 @@ describe('util > sha256', () => {
         expect(expected).toEqual(actual)
     })
 
-    test('sha256 関数が同じhost名を元に複数回呼んでも同じIDを出力できる', async () => {
-        const expected = await sha256("example.com")
-        const actual = await sha256("example.com")
+    test('sha256 関数が同じテンプレート名を元に複数回呼んでも同じIDを出力できる', async () => {
+        const expected = await sha256("テンプレート1")
+        const actual = await sha256("テンプレート1")
 
         expect(expected).toEqual(actual)
     })
 
-    test('sha256 関数が異なるhost名を元に複数回呼ぶと異なるIDを出力できる', async () => {
-        const expected = await sha256("example.com")
-        const actual = await sha256("a.example.com")
+    test('sha256 関数が異なるテンプレート名を元に複数回呼ぶと異なるIDを出力できる', async () => {
+        const expected = await sha256("テンプレート1")
+        const actual = await sha256("テンプレート2")
 
         expect(expected).not.toEqual(actual)
     })
